311bd0712b8ab03940f7cb14c9ab45e56cc6e358
Fauwaaz <fauwaazshaikh999@gmail.com>
2024-12-10 15:50:22 +0530

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center text-center  sm:items-start">
-        <h1 className="text-4xl font-bold">Hacker Write</h1>
-        <h2 className="text-center mt-[-20px]">Coming Soon</h2>
+        <h1 className="text-5xl font-bold">Hacker Write</h1>
+        <div className="w-full text-center">
+        <h2 className="mt-[-20px]">Coming Soon</h2>
+        </div>
       </main>
     </div>
   );
